bd518bc5a3c138782b0fbfdb641240c36dfd5768
holivane <holivane@gmail.com>
2024-07-10 22:50:51 -0300

    Repetição

---
+++ modulo_2/04_repeticao.rb
@@ -0,0 +1,13 @@
+i = 0
+num = 5
+
+# WHILE
+while i < num do
+  puts "Contando... #{i} enquanto for menor que #{num}"
+  i += 1
+end
+
+# EaCH
+(0..5).each do |i|
+  puts "Contando cada elemento do range: #{i}"
+end
